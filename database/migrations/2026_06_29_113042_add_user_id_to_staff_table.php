df47275eb209e572e183c034f1489989757ae45d
unknown <Humna Umar>
2026-07-01 14:31:45 +0500

    Staff database table updated added

---
+++ database/migrations/2026_06_29_113042_add_user_id_to_staff_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('staff', function (Blueprint $table) {
+            // user_id ka column add karein
+            $table->unsignedBigInteger('user_id')->nullable()->after('id');
+
+            // Foreign key relation (optional par best practice hai)
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('staff', function (Blueprint $table) {
+            //
+        });
+    }
+};
